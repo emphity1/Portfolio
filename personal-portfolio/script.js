9992f47e9ca5bd13b39492d62fe2f234ed4e755b
Dimitri <dimak821@gmail.com>
2022-10-01 18:41:40 +0200

    update
    almost finished

--- personal-portfolio/script.js
+++ personal-portfolio/script.js
@@ -7,22 +7,77 @@ switchElement.addEventListener('click', ()=>{
 })
 
 
+
+
 const sql = document.querySelector('#sql');
+const c = document.querySelector('#c');
+const godot = document.querySelector('#godot');
+const html = document.querySelector('#html');
+const css = document.querySelector('#css');
+const js = document.querySelector('#js');
 
-let intervalId;
-const delay = 100;
 
-
-
-function function1(){
-   intervalId = setInterval(()=> (sql.style.bottom += '20px',delay)); 
+/*
+function slideUp(){
+    sql.style.bottom = 20 + 'px';
+    
+   
 }
-
-function function2(){
-    intervalId = setInterval(()=> (sql.style.bottom = '-20px',delay)); 
-    clearInterval(intervalId);
+function slideDown(){
+    sql.style.bottom = -20 + 'px';
+    
 }
+*/
 
 
-sql.addEventListener('mouseover', function1);
-sql.addEventListener('mouseleave', function2);
+//SQL
+sql.addEventListener('mouseover', function(){
+    sql.style.bottom = 20 + 'px';
+});
+sql.addEventListener('mouseleave', function(){
+    sql.style.bottom = -20 + 'px';
+});
+
+//C
+c.addEventListener('mouseover', function(){
+    c.style.bottom = 300 + 'px';
+});
+c.addEventListener('mouseleave', function(){
+    c.style.bottom = 280 + 'px';
+});
+
+//Godot
+godot.addEventListener('mouseover', function(){
+    godot.style.bottom = 160 + 'px'
+});
+godot.addEventListener('mouseleave', function(){
+    godot.style.bottom = 140 + 'px'
+});
+
+
+//html
+html.addEventListener('mouseover', function(){
+    html.style.bottom = 40 + 'px'
+});
+html.addEventListener('mouseleave', function(){
+    html.style.bottom = 20 + 'px'
+});
+
+//css
+css.addEventListener('mouseover', function(){
+    css.style.bottom = 150 + 'px'
+});
+css.addEventListener('mouseleave', function(){
+    css.style.bottom = 130 + 'px'
+
+});
+
+//js
+js.addEventListener('mouseover', function(){
+    js.style.bottom = 260 + 'px'
+
+});
+js.addEventListener('mouseleave', function(){
+    js.style.bottom = 249 + 'px'
+
+});
