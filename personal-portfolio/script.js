210a5a2eaaea048da351aa1e0583190999d147fe
Dimitri <dimak821@gmail.com>
2022-10-01 17:06:38 +0200

    update

--- personal-portfolio/script.js
+++ personal-portfolio/script.js
@@ -1,8 +1,28 @@
 
 
-const switchElement = document.querySelector('.switch')
+const switchElement = document.querySelector('.switch');
 
 switchElement.addEventListener('click', ()=>{
     document.body.classList.toggle('dark')
 })
 
+
+const sql = document.querySelector('#sql');
+
+let intervalId;
+const delay = 100;
+
+
+
+function function1(){
+   intervalId = setInterval(()=> (sql.style.bottom += '20px',delay)); 
+}
+
+function function2(){
+    intervalId = setInterval(()=> (sql.style.bottom = '-20px',delay)); 
+    clearInterval(intervalId);
+}
+
+
+sql.addEventListener('mouseover', function1);
+sql.addEventListener('mouseleave', function2);
